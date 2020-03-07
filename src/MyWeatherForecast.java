18cd2caf9206bec713dc02dc9baa42a623b6c416
Amadey <stevedrumble1@gmail.com>
2020-03-07 14:30:40 +0300

    MyWeatherForecast is now a Singleton

--- src/MyWeatherForecast.java
+++ src/MyWeatherForecast.java
@@ -2,8 +2,21 @@ public class MyWeatherForecast implements WeatherForecast {
 
     private WeatherDB weatherDB;
 
-    MyWeatherForecast(WeatherDB weatherDB){
+    private static MyWeatherForecast obj = new MyWeatherForecast();
+
+    private MyWeatherForecast(){
+    }
+
+    public void setWeatherDB(WeatherDB weatherDB){
         this.weatherDB = weatherDB;
+    }
+
+    public WeatherDB getWeatherDB(){
+        return this.weatherDB;
+    }
+
+    public static MyWeatherForecast getInstance(){
+        return obj;
     }
 
     public ClientResponse predictWeatherForClient(ClientRequest request) {
